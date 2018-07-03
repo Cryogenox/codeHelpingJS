a276bc67faf3ae2418bc496ce9a7174bfab37dc1
Cryogenox <cryogenox@gmail.com>
2018-07-03 13:36:08 +0200

    Updated comments for more clarity in checkThis.js
    Comments have been updated to give a better idea of what each piece of code does. This might be especially useful for people wanting to use it as an exercise

--- checkThis.js
+++ checkThis.js
@@ -1,14 +1,14 @@
 function check(input){ 
-if(Object.prototype.toString.call(input) === '[object Array]') { //check if input is array
+if(Object.prototype.toString.call(input) === '[object Array]') { //check if the input is an array
     console.log( 'Array detected...' );
         var output = ''; //set space and var
-        for (var z in input) { //loop through array
-         output += z + " " + typeof z + ": " + input[z] + "\n"; //build up output
+        for (var z in input) { //loop through the array
+         output += z + " " + typeof z + ": " + input[z] + "\n"; //build up output of array items and their values
         }
         console.log('array:' + output); //output findings
 }
 
-else if(input !== null && typeof input === 'object'){ //check if input is object
+else if(input !== null && typeof input === 'object'){ //check if input is an object
     console.log('Object detected...') //signal detection
     console.log({input}); //print object as object
 }
@@ -22,7 +22,7 @@ else if(typeof input === "function") {
 else {
     console.log('Variable detected...') //signal detection
     if(typeof input === undefined){ //check if var is undefined
-        console.log('Variable is undefined.');  //signal undefined
+        console.log('Variable is undefined.');  //signal undefined variable
     }
     else {
         console.log('variable value: ' + input); //output variable value
